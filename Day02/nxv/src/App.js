5b37dbaf963e837b7f5495f03d8ffff758e962e2
VinhNguyen205 <realsteelworld2k5@gmail.com>
2025-02-16 02:42:33 +0700

    .

--- Day02/nxv/src/App.js
+++ Day02/nxv/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Trần Tiến Anh
+          Learn React Vinh
         </a>
       </header>
     </div>
