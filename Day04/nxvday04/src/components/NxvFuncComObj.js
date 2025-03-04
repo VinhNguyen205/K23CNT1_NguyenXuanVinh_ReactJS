05512448ac3a6944a3d35e98d9cbf2854098fb0d
VinhNguyen205 <realsteelworld2k5@gmail.com>
2025-03-04 10:18:24 +0700

    Update and rename NqtFuncComObj.js to NxvFuncComObj.js

---
+++ Day04/nxvday04/src/components/NxvFuncComObj.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function NxvFuncComObj(nxvProps) {
+return (
+    <div>
+      <h2>Props is Object</h2>
+      <p>
+        <b>
+        Info:
+        </b>
+      </p>
+      <p>FullName: {nxvProps.RenderInfo.fullName}</p>
+      <p>Age: {nxvProps.RenderInfo.age}</p>
+      <p>Note: {nxvProps.RenderInfo.note}</p>
+    </div>
+  )
+}
