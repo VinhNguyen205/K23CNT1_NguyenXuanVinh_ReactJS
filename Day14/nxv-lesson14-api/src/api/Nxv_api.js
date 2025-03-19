bed048925be9514a4df6ab90e29dcb6c829a31a5
Your Name <you@example.com>
2025-03-19 23:20:39 +0700

    upload pls

---
+++ Day14/nxv-lesson14-api/src/api/Nxv_api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const NxvAxiosUsers = axios.create({
+  baseURL: 'https://67da147735c87309f52add9a.mockapi.io/k23cnt1_nguyenxuanvinh_2310900117/', // Replace with your API base URL
+});
+
+export default NxvAxiosUsers;
